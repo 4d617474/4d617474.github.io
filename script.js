d67a1a43289e0bca2ee120981bf58fd4a38418cc
4d617474 <33104312+4d617474@users.noreply.github.com>
2017-12-19 23:16:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ toggleSP(1,1);
 function toggleSP(p1, p2) {
 	var spFrame = document.getElementById("spFrame");
 	var spFrameIn = document.getElementById("spFrame-In");
+	var spFrameLink = document.getElementById("spFrame-settings-github");
 	if (p1 == 1) {
 		spFrameIn.src = "startpages/startpage1/hp4Nox.html";
+		spFrameLink.href = "https://github.com/4d617474/startpage1";
 	} else if (p1 == 2) {
-		spFrameIn.src = "startpages/hpstarcitizen/hpsc.html";		
+		spFrameIn.src = "startpages/hpstarcitizen/hpsc.html";	
+		spFrameLink.href = "https://github.com/4d617474/hpstarcitizen";
 	}
 	if (p2 == 0) {
 		//expanding...
