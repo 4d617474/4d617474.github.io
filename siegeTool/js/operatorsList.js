1f4ef712a11af14c4b1826c9c09481fed235d215
4d617474 <33104312+4d617474@users.noreply.github.com>
2019-04-30 03:43:46 +0200

    Update operatorsList.js

--- siegeTool/js/operatorsList.js
+++ siegeTool/js/operatorsList.js
@@ -1,8 +1,3 @@
-/*var operators = [mozzie,gridlock,nomad,kaid,clash,maverick,maestro,alibi,lion,
-finka,vigil,dokkaebi,zofia,ela,ying,lesion,mira,jackal,hibana,echo,caveira,
-capitao,blackbeard,buck,frost,mute,sledge,smoke,thatcher,ash,castle,pulse,
-thermite,montagne,rook,twitch,doc,rook,jager,bandit,blitz,iq,fuze,glaz,
-tachanka,kapkan];*/
 var operators = [
 //Attacking Side
 "Gridlock","Nomad","Maverick","Lion","Finka","Dokkaebi","Zofia","Ying","Jackal",
@@ -12,61 +7,4 @@ var operators = [
 "Mozzie","Kaid","Clash","Maestro","Alibi","Vigil","Ela","Lesion","Mira","Echo",
 "Caveira","Frost","Mute","Smoke","Castle","Pulse","Rook","Doc","Jager",
 "Bandit","Tachanka","Kapkan"];
-
-//ABCDEFGHIJKLMNOPQRSTUVWXYZ
-/*
-var AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,AW,
-  AX,AY,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT;
-AA = ["kapkan"];//DEF
-AB = ["tachanka"];//DEF
-AC = ["glaz"];
-AD = ["fuze"];
-AE = ["iq"];
-AF = ["blitz"];
-AG = ["bandit"];//DEF
-AH = ["jager"];//DEF
-AI = ["rook"];//DEF
-AJ = ["doc"];//DEF
-AK = ["twitch"];
-AL = ["montagne"];
-AM = ["thermite"];
-AN = ["pulse"];//DEF
-AO = ["castle"];//DEF
-AP = ["ash"];
-AQ = ["thatcher"];
-AR = ["smoke"];//DEF
-AS = ["sledge"];
-AT = ["mute"];//DEF
-AU = ["frost"];//DEF
-AV = ["buck"];
-AW = ["valkyrie"];//DEF
-AX = ["blackbeard"];
-AY = ["capitao"];
-AZ = ["caveira"];//DEF
-BA = ["echo"];//DEF
-BB = ["hibana"];
-BC = ["jackal"];
-BD = ["mira"];//DEF
-BE = ["lesion"];//DEF
-BF = ["ying"];
-BG = ["ela"];//DEF
-BH = ["zofia"];
-BI = ["dokkaebi"];
-BJ = ["vigil"];//DEF
-BK = ["finka"];
-BL = ["lion"];
-BM = ["alibi"];//DEF
-BN = ["maestro"];//DEF
-BO = ["maverick"];
-BP = ["clash"];//DEF
-BQ = ["kaid"];//DEF
-BR = ["nomad"];
-BS = ["gridlock"];
-BT = ["mozzie"];//DEF
-
-var operatorsATK = [AC,AD,AE,AF,AK,AL,AM,AP,AQ,AS,AV,AX,AY,BB,BC,BF,BH,BI,BK,BL,
-  BO,BR,BS];
-
-var operatorsDEF = ["","","","","","","","","","","","","","","","","","","","",
-"","","",AA,AB,AG,AH,AI,AJ,AN,AO,AR,AT,AU,AW,AZ,BA,BD,BE,BG,BJ,BM,BN,BP,BQ,BT];
-*/
+//44
