255eb800156a0ad06210321910faf60d90cb6a49
4d617474 <33104312+4d617474@users.noreply.github.com>
2019-05-01 19:24:30 +0200

    o

--- siegeTool/js/mapInfo.js
+++ siegeTool/js/mapInfo.js
@@ -1,15 +1,19 @@
-mapInfoSetTitle();
+//mapInfoSetTitle();
+var miTitleA = document.getElementById("mapInfoTitleA");
+var miTitleB = document.getElementById("mapInfoTitleB");
+var miMapSelection = document.getElementById("mapSelection");
+var miModeSelection = document.getElementById("modeSelection");
+var miResultScreen = document.getElementById("resultScreen");
+var mapSelected, modeSelected;
 resetB();
 setMaps();
 
 function resetB(){
-  var mImapSelection = document.getElementById("mapSelection");
-  var mImodeSelection = document.getElementById("modeSelection");
-  var mIresultScreen = document.getElementById("resultScreen");
-  mImapSelection.style.display = "none";
-  mImodeSelection.style.display = "none";
-  mIresultScreen.style.display = "none";
-  mImapSelection.style.display = "block";
+  miMapSelection.style.display = "none";
+  miModeSelection.style.display = "none";
+  miResultScreen.style.display = "none";
+  miMapSelection.style.display = "block";
+  miTitleA.innerHTML = "<br>" + "Select a map";
 }
 function setMaps()
 {
@@ -21,13 +25,37 @@ function setMaps()
     //console.log(mapsThumb[i]);
     var dummy = mapThumb[i];
     dummy.style.backgroundImage = 'url("thumbnails/maps/' + maps[i] + '.png")';
-    mapName[i].innerHTML = maps[i];
+    dummy.innerHTML = maps[i];
+    //mapName[i].innerHTML = maps[i];
   }
 }
-function mapInfoSetTitle(){
-  var mITitle = document.getElementById("mapInfoTitle");
-  mITitle.innerHTML = "<br>" + "Select a map";
+function mapSelection()
+{
+  //console.log(event.target.innerHTML);
+  mapSelected = event.target.innerHTML;
+  document.getElementById("mapSelection").style.display = "none";
+  document.getElementById("modeSelection").style.display = "block";
+  miTitleA.innerHTML = "<br>" + "Select a mode";
+  miTitleB.innerHTML = "<br>" + mapSelected;
 }
-function mapSelection(){
+function modeSelection()
+{
+  //console.log(event.target.innerHTML);
+  modeSelected = event.target.innerHTML;
+  document.getElementById("modeSelection").style.display = "none";
+  document.getElementById("spawnSelection").style.display = "block";
+  miTitleA.innerHTML = "<br>" + "Select a location";
+  miTitleB.innerHTML = "<br>" + mapSelected + " " + modeSelected;
+}
+function spawnSelection()
+{
+  spawnSelected = event.target.innerHTML;
+  document.getElementById("spawnSelection").style.display = "none";
+  document.getElementById("resultScreen").style.display = "block";
+  miTitleA.innerHTML = "<br>" + "Result";
+  miTitleB.innerHTML = "<br>" + mapSelected + " " + modeSelected + " " + spawnSelected;
+}
+function resultScreen()
+{
 
 }
--- siegeTool/js/mapsList.js
+++ siegeTool/js/mapsList.js
@@ -3,3 +3,66 @@ var maps = ["bank","border","chalet","club house","coastline","consulate",
 "outback","presidential plane","skyscraper","theme park","tower","villa","yacht"
 ];
 //20
+
+//map = [
+  //modeHostage
+  //site1
+//"floor & name",
+//"cellingHatches","floorHatches","reinforcableWalls","doors","windows"
+  //site2
+//"floor & name",
+//"cH","fH","rW","d","w"
+  //site3
+//"floor & name",
+//"cH","fH","rW","d","w"
+  //site4
+//"floor & name",
+//"cH","fH","rW","d","w"
+
+//modeSecure
+  //site1
+//"cH","fH","rW","d","w"
+  //site2
+//"cH","fH","rW","d","w"
+  //site3
+//"cH","fH","rW","d","w"
+  //site4
+//"cH","fH","rW","d","w"
+
+  //modeBomb
+  //site1
+  //site
+//"cH","fH","rW","d","w"
+  //connectors
+//"cH","fH","rW","d","w"
+
+  //site2
+  //site
+//"cH","fH","rW","d","w"
+  //connectors
+//"cH","fH","rW","d","w"
+
+  //site3
+  //site
+//"cH","fH","rW","d","w"
+  //connectors
+//"cH","fH","rW","d","w"
+
+  //site4
+  //site
+//"cH","fH","rW","d","w"
+  //connectors
+//"cH","fH","rW","d","w"
+
+//];
+
+
+//bank = ["1","0","4","2","3","1","0","4","2","3","1","0","4","2","3",
+//"1","0","4","2","3","1","0","4","2","3","1","0","4","2","3",
+//"1","0","4","2","3","1","0","4","2","3","1","0","4","2","3",
+//"1","0","4","2","3","1","0","4","2","3","1","0","4","2","3",
+//"1","0","4","2","3","1","0","4","2","3","1","0","4","2","3",
+//"1","0","4","2","3",]
+/*
+= ["","","","","",  "",""]
+*/
