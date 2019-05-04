40043e52c68461210b3f86704cb093849fc79c92
4d617474 <33104312+4d617474@users.noreply.github.com>
2019-05-04 20:30:23 +0200

    -

--- siegeTool/js/menu.js
+++ siegeTool/js/menu.js
@@ -1,12 +1,18 @@
 var menuA = document.getElementById("operatorSelectBoard");
 var menuB = document.getElementById("mapInfo");
+var menuOP = document.getElementById("menuOperators");
+var menuMA = document.getElementById("menuMap");
 function menuSelect()
 {
   if (event.target.id == 'menuOperators'){
     menuA.style.display = "block";
+    menuOP.style.backgroundColor ="#353540";
+    menuMA.style.backgroundColor ="#212129";
     menuB.style.display = "none";
   } else if (event.target.id == 'menuMap'){
     menuB.style.display = "block";
+    menuMA.style.backgroundColor ="#353540";
+    menuOP.style.backgroundColor ="#212129";
     menuA.style.display = "none";
   }
 }
